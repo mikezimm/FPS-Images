18c6cefb8ee44958e834e93df2a02374f0401f5c
mikezimm <49648086+mikezimm@users.noreply.github.com>
2023-04-21 21:01:38 -0400

    Initial yo build

---
+++ src/webparts/fpsImages/components/IFpsImagesProps.ts
@@ -0,0 +1,7 @@
+export interface IFpsImagesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/fpsImages/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IFpsImagesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'FpsImagesWebPartStrings' {
+  const strings: IFpsImagesWebPartStrings;
+  export = strings;
+}
